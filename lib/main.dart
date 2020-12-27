71b93d80e1f1ec5d6d6006696c15b1abbdd3495e
Mostafa-Fathi <mostafafathy677@gmail.com>
2020-12-27 19:40:27 +0200

    edit
    edit

--- lib/main.dart
+++ lib/main.dart
@@ -2,37 +2,31 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-      title: 'Flutter RTL',
-      color: Colors.white,
-      builder: (BuildContext context, Widget child) {
-        return new Directionality(
-          textDirection: TextDirection.rtl,
-          child: new Builder(
-            builder: (BuildContext context) {
-              return new MediaQuery(
-                data: MediaQuery.of(context).copyWith(
-                  textScaleFactor: 1.0,
-                ),
-                child: ListView(
-                  children: [
-                    usingImg()
-                  ],
-                ),
-              );
-            },
-          ),
-        );
-      },
+    title: 'Flutter RTL',
+    theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.indigo,
+        backgroundColor: Colors.white,
+        accentColor: Colors.indigoAccent),
+    debugShowCheckedModeBanner: false,
 
+    home:ListView(
+
+      children: [],
+    ),
+    /*Directionality(
+      textDirection: TextDirection.rtl,
+      child: ListView(
+        children: [usingImg()],
+      ),
+    ),*/
   ));
 }
-
 
 Widget usingImg() {
   AssetImage assetImage = AssetImage('images/header.png');
   Image image = Image(image: assetImage);
   return Container(
     child: image,
-
   );
 }
